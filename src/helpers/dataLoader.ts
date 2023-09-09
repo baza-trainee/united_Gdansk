2c70ad5454a41bc08e4915c971f536021d9b34d0
miamarichka <ilchyshyn.marichka@gmail.com>
2023-09-09 23:57:08 +0300

    adding lang functionality

---
+++ src/helpers/dataLoader.ts
@@ -0,0 +1,10 @@
+import { Lang } from '../types/langTypes';
+
+
+export const loadData = (selectedLang:Lang) => {
+  if (selectedLang === 'UKR') {
+    return import('../content/dataUA.json');
+  } else {
+    return import('../content/dataPL.json');
+  }
+};
--- src/sections/App.tsx
+++ src/sections/App.tsx
@@ -1,8 +1,36 @@
+import {useEffect, useState} from 'react'
+import { loadData } from '../helpers/dataLoader';
+import { Lang } from '../types/langTypes';
+import { Content } from '../types/contentType';
+import useLanguage from '../hooks/useLanguage';
+
 function App() {
+  const [lang, setLang] = useLanguage();
+  const [content, setContent] = useState<Content| null>(null)
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await loadData(lang);
+      setContent(data);
+    }
+
+    fetchData();
+  }, [lang])
+  
 
   return (
     <>
-      <div></div>
+      <div>
+        <button type="button"
+          onClick={()=> setLang(Lang.UA)}
+        >UKR</button>
+        <br />
+        <button type="button"
+          onClick={()=> setLang(Lang.PL)}
+        >PL</button>
+      </div>
+      <h1>{content?.title}</h1>
+      <p>{content?.about}</p>
     </>
   )
 }
