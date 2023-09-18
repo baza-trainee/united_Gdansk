af4200e7689d5b50272fbed0b326bd9f0129e642
Hennadii Borysevych <genuch3@gmail.com>
2023-09-19 00:40:39 +0200

    feat:AboutRallies component and changes to Gallery

--- src/types/contentType.ts
+++ src/types/contentType.ts
@@ -2,4 +2,11 @@ export type Content = {
   headerButton: string[];
   title: string;
   about: string;
+  aboutRollies: {
+    title: string;
+    text: string;
+  };
+  aboutGallery: {
+    title: string;
+  };
 };
