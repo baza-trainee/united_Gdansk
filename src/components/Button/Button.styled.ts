89c5d6e88357871e0ff5258fd91819d6fbd41eb8
shaman2507 <alabay174@gmail.com>
2023-10-08 19:43:48 +0200

    Blick added
    Blick added to donation btn

--- src/components/Button/Button.styled.ts
+++ src/components/Button/Button.styled.ts
@@ -1,9 +1,58 @@
 import styled from '@emotion/styled';
 
+export const DonateBtnsSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+
+  @media screen and (min-width: 768px) {
+    gap: 48px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    gap: 68px;
+  }
+`;
+
+export const BlickBtnWrapper = styled.div`
+  
+  
+`;
+
+export const BlickSvg = styled.svg`
+  width: 44px;
+  height: 20px;
+  fill: #fff;
+
+  @media screen and (min-width: 768px) {
+    width: 60px;
+    height: 30px;
+  }
+
+`;
+
+export const BlickBtn = styled.button`
+  width: 62px;
+  height: 44px;
+  background-color: #000000;
+  border-radius: 5px;
+
+  @media screen and (min-width: 768px) {
+    width: 86px;
+    height: 60px;
+  }
+
+`;
+
+export const DonateBtnWrapper = styled.div`
+
+`;
+
 export const Btn = styled.button`
   display: block;
-  width: 280px;
-  height: 72px;
+  width: 196px;
+  height: 64px;
   border-radius: 12px;
   padding: 16px;
   border: 5px solid #000000;
